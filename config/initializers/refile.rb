d1e6b2ff0589823edce1a13a321a413f5db2c597
Marlon Bernardes <marlonjb.cb@gmail.com>
2015-04-11 18:59:32 -0300

    upload: validar parametros do ambiente

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,5 +1,8 @@
 require "refile/backend/s3"
 
+raise "Variável de ambiente AWS_KEY_ID não informada" if ENV["AWS_KEY_ID"].nil?
+raise "Variável de ambiente AWS_ACCESS_KEY não informada" if ENV["AWS_ACCESS_KEY"].nil?
+
 aws = {
   access_key_id: ENV["AWS_KEY_ID"],
   secret_access_key: ENV["AWS_ACCESS_KEY"],
